2c16d3302c83d683031bfb25cdeca9c221532003
mcgrawga <garthmcgraw@statgolf.com>
2016-06-07 12:44:17 -0500

    Trying to configure password recovery in prod.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "statgolf.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "garthmcgraw@statgolf.com",
+  password: "Sher1lock*"
+}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
