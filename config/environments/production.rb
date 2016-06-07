33ca4539d9e24692a324f2f96d4425ac74be88db
mcgrawga <garthmcgraw@statgolf.com>
2016-06-07 13:15:49 -0500

    more email stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "statgolf.com",
+  domain: "statgolf.herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: "support@statgolf.com",
