1eeeca8e28d7cbe6f5ab28a1c074361166e19e20
mcgrawga <garthmcgraw@statgolf.com>
2016-12-03 21:03:45 -0600

    Added more validation around gir, fir and penalties

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,12 +3,62 @@ class Score < ActiveRecord::Base
 
 	validates 	:score_hole_1, :score_hole_2, :score_hole_3, :score_hole_4, 
 				:score_hole_5, :score_hole_6, :score_hole_7, :score_hole_8, 
-				:score_hole_9, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 10 }
+				:score_hole_9, :score_hole_10, :score_hole_11, :score_hole_12, 
+				:score_hole_13, :score_hole_14, :score_hole_15, :score_hole_16, 
+				:score_hole_17, :score_hole_18, 
+				:numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 10 }, allow_nil: true
 	validates 	:putts_hole_1, :putts_hole_2, :putts_hole_3, :putts_hole_4, 
 				:putts_hole_5, :putts_hole_6, :putts_hole_7, :putts_hole_8, 
-				:putts_hole_9, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10 }
+				:putts_hole_9, :putts_hole_10, :putts_hole_11, :putts_hole_12, 
+				:putts_hole_13, :putts_hole_14, :putts_hole_15, :putts_hole_16, 
+				:putts_hole_17, :putts_hole_18, 
+				:numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10 }, allow_nil: true
 	validate :front9_back9_or_both
 	validate :putts
+	validate :gir
+	validate :fir
+	validates :penalties, :numericality => { :greater_than_or_equal_to => 0 }, allow_nil: true
+
+	def gir
+		if (tee.par_hole_10.blank?)  #9 hole
+			if (!greens_in_regulation.blank? && greens_in_regulation > 9)
+				errors.add(:base, "You can't hit more greens in regulation than holes you played.")
+				return
+			end
+		else  #18 hole
+			holes_played = 0
+			(1..18).each do |i|
+				score_hole = "score_hole_" + i.to_s
+	   			if (!send(score_hole).blank?)
+	   				holes_played = holes_played + 1
+	   			end
+			end
+			if (!greens_in_regulation.blank? && greens_in_regulation > holes_played)
+				errors.add(:base, "You can't hit more greens in regulation than holes you played.")
+				return
+			end
+		end
+	end
+
+	def fir
+		num_fairways = 0
+		num_holes = 18
+		if (tee.par_hole_10.blank?)  #9 hole
+			num_holes = 9
+		end
+		(1..num_holes).each do |i|
+			par_hole = "par_hole_" + i.to_s
+			score_hole = "score_hole_" + i.to_s
+			log("Hole: " + i.to_s + ", par: " + tee.send(par_hole).to_s + ", score: " + send(score_hole).to_s)
+   			if ( (!tee.send(par_hole).blank?) && (tee.send(par_hole) > 3) && (!send(score_hole).blank?))
+   				num_fairways = num_fairways + 1
+   			end
+		end
+		if (!fairways_hit.blank? && fairways_hit > num_fairways)
+			errors.add(:base, "You can't hit more fairways than the number of par 4s and par 5s you played.")
+			return
+		end
+	end
 
 	def front9_back9_or_both
 		if (tee.par_hole_10.blank?)  #9 hole
