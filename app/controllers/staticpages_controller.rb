40bb80297dd007b7645c45d599ad891c42322876
mcgrawga <garthmcgraw@statgolf.com>
2017-01-20 08:28:19 -0600

    abc testing landing pages

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,5 +1,14 @@
 class StaticpagesController < ApplicationController
   def index
+  	num = rand(1..3)
+  	log(num.to_s)
+  	if num == 1
+  		render :index
+  	elsif num == 2
+  		render :indexb
+  	else
+  		render :indexc
+  	end
   end
   def indexb
   end
